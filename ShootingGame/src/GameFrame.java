97b6c18a2e62f1021333e0a7b06db2bcf3c17eab
takanokei01 <zeal23410219@fsgcl.jp>
2024-06-17 11:08:54 +0900

    10.6

--- ShootingGame/src/GameFrame.java
+++ ShootingGame/src/GameFrame.java
@@ -14,7 +14,12 @@ public class GameFrame extends MyFrame {
 				PlayerBullet b=GameWorld.playerBullets.get(i);
 				b.draw(this);
 				b.move();
-				i++;
+				if(b.y<0) {
+					GameWorld.playerBullets.remove(i);
+				}
+				else {
+					i++;
+				}
 			}
 			sleep(0.03);
 		}
--- ShootingGame/src/Player.java
+++ ShootingGame/src/Player.java
@@ -35,6 +35,7 @@ public class Player extends Character implements KeyListener {
 		}
 		if(e.getKeyCode()==KeyEvent.VK_SPACE) {
 			GameWorld.playerBullets.add(new PlayerBullet(x,y,0,-10));
+			System.out.println("弾の数="+ GameWorld.playerBullets.size());
 		}
 	}
 	public void move() {
