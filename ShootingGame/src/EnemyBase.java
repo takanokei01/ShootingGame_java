c6094f3feff54e41ab3f00eb81a7f6718b6a3180
takanokei01 <zeal23410219@fsgcl.jp>
2024-06-18 10:46:51 +0900

    11.4

--- ShootingGame/src/EnemyBase.java
+++ ShootingGame/src/EnemyBase.java
@@ -1,5 +1,11 @@
 
 public class EnemyBase extends Enemy {
+	public void draw(MyFrame f) {
+		f.setColor(0, 128,0);
+		f.fillOval(x, y, 32, 32);
+		f.setColor(200,200,200);
+		f.fillOval(x-16, y+8, 64, 16);
+	}
 	public EnemyBase(double x, double y, double vx, double vy) {
 		super(x, y, vx, vy);
 	}
@@ -9,6 +15,9 @@ public class EnemyBase extends Enemy {
 			vx=-1;
 		if(x<100)
 			vx=1;
+		if(Math.random()<0.05) {
+			GameWorld.enemies.add(new StraightEnemy(x,y,0,2));
+		}
 	}
 
 }
