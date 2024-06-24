96d2ee5e03baaff33b3a93112894d938babed116
takanokei01 <zeal23410219@fsgcl.jp>
2024-06-24 10:44:11 +0900

    12.1&2

--- ShootingGame/src/GameFrame.java
+++ ShootingGame/src/GameFrame.java
@@ -13,6 +13,8 @@ public class GameFrame extends MyFrame {
 			GameWorld.player.move();
 			movePlayerBullets();
 		    moveEnemies();
+		    checkPlayerAndEnemies();
+		    checkPlayerBulletsAndEnemies();
 			sleep(0.03);
 		}
 	}
@@ -38,5 +40,41 @@ public class GameFrame extends MyFrame {
 			}
 		}
 	}
+		public void checkPlayerAndEnemies() {
+			 for(int i=0; i<GameWorld.enemies.size(); i++) {
+					Enemy e=GameWorld.enemies.get(i);
+					if(Math.abs(e.x-GameWorld.player.x)<=30 &&
+					   Math.abs(e.y-GameWorld.player.y)<=30) {
+						System.out.println("やられた！");
+						GameWorld.player.y=-1000;
+					}
+		}
+	}
+	    public void checkPlayerBulletsAndEnemies() {
+	    	int i=0;
+	    	while(i<GameWorld.playerBullets.size()){
+	    		PlayerBullet b=GameWorld.playerBullets.get(i);
+	    		int j=0;
+	    		int hits=0;
+	    		while(j<GameWorld.enemies.size()) {
+	    			Enemy e=GameWorld.enemies.get(j);
+	    			if(Math.abs(e.x-b.x)<=30 &&
+	    					Math.abs(e.y-b.y)<=30) {
+	    				System.out.println("あたり");
+	    				hits++;
+	    				GameWorld.enemies.remove(j);
+	    			}	else {
+	    				j++;
+	    			}
+	    		}
+	    		if(hits>0) {
+	    			GameWorld.playerBullets.remove(i);
+	    		}else {
+	    			i++;
+	    		}
+	    	}
+	    }
+	
+	}
 
-}
+
