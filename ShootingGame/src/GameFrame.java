468c0940218e1f72a259a37e9a366edfca0b28b3
takanokei01 <zeal23410219@fsgcl.jp>
2024-06-25 09:49:25 +0900

    13.5

--- ShootingGame/src/GameFrame.java
+++ ShootingGame/src/GameFrame.java
@@ -4,13 +4,17 @@ public class GameFrame extends MyFrame {
 	public void run() {
 		GameWorld.player= new Player(100, 300, 0, 0);
 		addKeyListener(GameWorld.player);
+		GameWorld.stage=1;
 		while(true) {
+			GameWorld.player.x=100;
+			GameWorld.player.y=300;
 			GameWorld.playerBullets=new Vector<PlayerBullet>();
 			GameWorld.enemies=new Vector<Enemy>();
 			GameWorld.enemies.add(new EnemyBase(100, 50, 1, 0));
 			GameWorld.enterPressed=false;
 			while(true) {
 				clear();
+				drawString("Stage = "+GameWorld.stage,300,50,15)
 				GameWorld.player.draw(this);
 				GameWorld.player.move();
 				movePlayerBullets();
@@ -21,12 +25,14 @@ public class GameFrame extends MyFrame {
 					setColor(0,0,0);
 					drawString("クリア！",100,200,40);
 					if(GameWorld.enterPressed) {
+						GameWorld.stage++;
 						break;
 					}
 				} else if(GameWorld.player.y<0) {
 					setColor(0,0,0);
 					drawString("ゲームオーバー！",50,200,40);
 					if(GameWorld.enterPressed) {
+						GameWorld.stage=1;
 						break;
 					}
 				}
